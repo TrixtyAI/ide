20b1cb3feab752d3d6a5ec4142032729aa1c4e89
jmaxdev <251231531+jmaxdev@users.noreply.github.com>
2026-05-02 00:12:09 -0300

    fixed fmt

--- apps/desktop/src-tauri/src/discord_rpc.rs
+++ apps/desktop/src-tauri/src/discord_rpc.rs
@@ -5,10 +5,6 @@ use log::{error, info, warn};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use tauri::{AppHandle, Emitter};
-#[cfg(unix)]
-use std::env;
-#[cfg(unix)]
-use std::path::PathBuf;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 #[cfg(unix)]
 use tokio::net::UnixStream;
